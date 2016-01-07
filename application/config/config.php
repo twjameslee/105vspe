5f729337b58a94e605e6695263db1a873992a463
james <james@chsc.tw>
2016-01-07 17:25:25 +0800

    add MySQL config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://nas.chsc.tw/~james/vspe/105/';
 
 /*
 |--------------------------------------------------------------------------
@@ -314,7 +314,7 @@ $config['cache_query_string'] = FALSE;
 | http://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'i_love_james_very_much';
 
 /*
 |--------------------------------------------------------------------------
