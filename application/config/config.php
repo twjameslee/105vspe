75d5df4ff48f0a7ab2cdde40dfb7d859a7f268ec
James Lee <tw.jameslee@gmail.com>
2016-03-26 14:40:33 +0800

    upload control

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://nas.chsc.tw/~james/vspe/105/';
+$config['base_url'] = 'http://nas.chsc.tw/~vspe/105/';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/routes.php
+++ application/config/routes.php
@@ -52,8 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['news'] = 'news/index';
-$route['news/(:any)'] = 'news/view/$1';
+//$route['news'] = 'news/index';
+//$route['news/(:any)'] = 'news/view/$1';
 
-$route['(:any)'] = 'pages/view/$1';
-$route['default_controller'] = 'pages/view';
+//$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'upload/index';
